41947274159c325f7fe95eedd52e59577207500d
Qian <qian.fu@outlook.com>
2017-05-26 13:44:47 +0100

    Add a function

--- code/signal_boxes.py
+++ code/signal_boxes.py
@@ -3,10 +3,11 @@
 import os
 import string
 
+import bs4
 import pandas as pd
 import requests
 
-from utilities import cdd_rc_dat, load_pickle, save_pickle, get_last_updated_date, parse_table
+from utilities import cdd_rc_dat, load_pickle, save_pickle, get_last_updated_date, parse_table, parse_tr
 
 
 #
@@ -76,3 +77,52 @@ def get_signal_box_prefix_codes(update=False):
         save_pickle(signal_box_prefix_codes, path_to_file)
 
     return signal_box_prefix_codes
+
+
+def scrape_non_national_rail_codes():
+    url = 'http://www.railwaycodes.org.uk/signal/signal_boxesX.shtm'
+    source = requests.get(url)
+    web_page_text = bs4.BeautifulSoup(source.text, 'lxml')
+    non_national_rail = [k.text for k in web_page_text.find_all('h3')]
+
+    non_national_rail_codes = {}
+    for n in non_national_rail:
+        sub_source = web_page_text.find(text=n)
+
+        # Find text descriptions
+        desc = sub_source.find_next('p')
+        desc_text = desc.text
+        while desc.find_next('p').text != '\xa0' and ' | ' not in desc.find_next('p').text:
+            desc_text = ' '.join([desc_text, desc.find_next('p').text])
+            desc = desc.find_next('p')
+
+        # Get table data
+        tbl = sub_source.find_next('table')
+        if tbl.find_previous('h3').text == n:
+            # header, dat = [th.text for th in tbl.find_all('th')], [td.text for td in tbl.find_all('td')[2:]]
+            # data = pd.DataFrame([dat[x:x + len(header)] for x in range(0, len(dat), len(header))], columns=header)
+            header = [th.text for th in tbl.find_all('th')]
+            dat = parse_tr(header, tbl.find_all('tr')[3:])
+            data = pd.DataFrame(dat, columns=header)
+        else:
+            data = None
+
+        non_national_rail_codes.update({n: {'Codes': data, 'Description': desc_text.replace('\xa0', '')}})
+
+    return non_national_rail_codes
+
+
+def get_non_national_rail_codes(update=False):
+    path_to_file = cdd_sig_box("Non-national-rail-signals.pickle")
+    if os.path.isfile(path_to_file) and not update:
+        non_national_rail_codes = load_pickle(path_to_file)
+    else:
+        try:
+            non_national_rail_codes = scrape_non_national_rail_codes()
+        except Exception as e:
+            print("Getting non-national rail signal codes ... failed due to {}.".format(e))
+            non_national_rail_codes = None
+
+        save_pickle(non_national_rail_codes, path_to_file)
+
+    return non_national_rail_codes
