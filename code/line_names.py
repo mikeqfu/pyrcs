0c089c816a365b414b04ca423a5a8f5adf1a38bd
mikeqfu <qian.fu@outlook.com>
2018-08-06 15:48:27 +0100

    Updated

--- code/line_names.py
+++ code/line_names.py
@@ -6,8 +6,7 @@ import re
 import pandas as pd
 import requests
 
-from utils import cdd, load_pickle, save_pickle, get_last_updated_date, parse_table
-
+from utils import cdd, get_last_updated_date, load_pickle, parse_table, save_pickle
 
 # ====================================================================================================================
 """ Change directory """
@@ -25,6 +24,26 @@ def cdd_line_names(*directories):
 """ Scrape/get data """
 
 
+def parse_route(x):
+    if 'Watford - Euston suburban route' in x:
+        route, route_note = 'Watford - Euston suburban route', x
+    elif ', including Moorgate - Farringdon' in x:
+        route_note = 'including Moorgate - Farringdon'
+        route = x.replace(', including Moorgate - Farringdon', '')
+    elif re.match('.+(?= \[\')', x):
+        route, route_note = re.split(' \[\'\(?', x)
+        route_note = route_note.strip(')\'\]')
+    elif re.match('.+\)$', x):
+        if re.match('.+(?= - \()', x):
+            route, route_note = x, None
+        else:
+            route, route_note = re.split(' \(\[?\'?', x)
+            route_note = route_note.rstrip('\'])')
+    else:
+        route, route_note = x, None
+    return route, route_note
+
+
 # Railway line names
 def scrape_line_names():
     """
@@ -37,27 +56,7 @@ def scrape_line_names():
 
     source = requests.get(url)
     row_lst, header = parse_table(source, parser='lxml')
-    line_names_data = pd.DataFrame([[r.replace('\xa0', '').strip() for r in row] for row in row_lst],
-                                   columns=header)
-
-    def parse_route(x):
-        if 'Watford - Euston suburban route' in x:
-            route, route_note = 'Watford - Euston suburban route', x
-        elif ', including Moorgate - Farringdon' in x:
-            route_note = 'including Moorgate - Farringdon'
-            route = x.replace(', including Moorgate - Farringdon', '')
-        elif re.match('.+(?= \[\')', x):
-            route, route_note = re.split(' \[\'\(?', x)
-            route_note = route_note.strip(')\'\]')
-        elif re.match('.+\)$', x):
-            if re.match('.+(?= - \()', x):
-                route, route_note = x, None
-            else:
-                route, route_note = re.split(' \(\[?\'?', x)
-                route_note = route_note.rstrip('\'])')
-        else:
-            route, route_note = x, None
-        return route, route_note
+    line_names_data = pd.DataFrame([[r.replace('\xa0', '').strip() for r in row] for row in row_lst], columns=header)
 
     line_names_data[['Route', 'Route_note']] = line_names_data.Route.map(parse_route).apply(pd.Series)
 
@@ -68,14 +67,15 @@ def scrape_line_names():
 
 # Get data of line names
 def get_line_names(update=False):
-    path_to_file = cdd_line_names("Line-names.pickle")
-    if os.path.isfile(path_to_file) and not update:
-        line_names = load_pickle(path_to_file)
+    pickle_filename = "Line-names.pickle"
+    path_to_pickle = cdd_line_names(pickle_filename)
+    if os.path.isfile(path_to_pickle) and not update:
+        line_names = load_pickle(path_to_pickle)
     else:
         try:
             line_names = scrape_line_names()
-            save_pickle(line_names, path_to_file)
+            save_pickle(line_names, path_to_pickle)
         except Exception as e:
-            print("Getting line names ... failed due to '{}'".format(e))
+            print("Failed to get line names due to \"{}\"".format(e))
             line_names = None
     return line_names
