4420edd3bcf5f70d4ec8fab9de74eca1cf26b1a7
mikeqfu <qian.fu@outlook.com>
2019-06-18 20:09:25 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
+import os
+
 import setuptools
+
+
+def find_all_pkg_dat_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+dat_files = find_all_pkg_dat_files('pyrcs\\dat')
+
 
 with open("README.md", 'r') as readme:
     long_description = readme.read()
 
 setuptools.setup(
 
-    name='pyrcscraper',
-    version='0.0.1',
+    name='pyrcs',
+    version='0.0.9',
 
     author='Qian Fu',
     author_email='qian.fu@outlook.com',
 
-    description=" A little web scraping tool for collecting railway codes used in GB.",
+    description=" A small toolkit for collecting railway codes used in GB.",
     long_description=long_description,
     long_description_content_type="text/markdown",
 
@@ -19,14 +33,14 @@ setuptools.setup(
 
     install_requires=[
         'beautifulsoup4',
-        'fuzzywuzzy',
         'html5lib',
         'lxml',
         'measurement',
         'more-itertools',
-        'numpy',
         'pandas',
-        'pdfkit',
+        'pyhelpers',
+        'python-dateutil',
+        'python-Levenshtein',
         'python-rapidjson',
         'requests',
         'sqlalchemy',
@@ -35,12 +49,19 @@ setuptools.setup(
 
     packages=setuptools.find_packages(exclude=["*.tests", "tests.*", "tests"]),
 
-    package_data={"pyrcscraper": ["dat/*"]},
+    package_data={"pyrcs": dat_files + ["line_data_cls/*", "other_assets_cls/*"]},
     include_package_data=True,
 
     classifiers=[
-        'License :: OSI Approved :: MIT',
-        'Programming Language :: Python :: 3.x',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: Microsoft :: Windows :: Windows 8',
+        'Operating System :: Microsoft :: Windows :: Windows 8.1',
         'Operating System :: Microsoft :: Windows :: Windows 10',
     ],
 )
