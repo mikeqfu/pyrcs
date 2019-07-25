551e698a10ec901e560abb622037885e1148235f
mikeqfu <qian.fu@outlook.com>
2019-07-25 13:13:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", 'r') as readme:
 setuptools.setup(
 
     name='pyrcs',
-    version='0.1.7',
+    version='0.1.19',
 
     author='Qian Fu',
     author_email='qian.fu@outlook.com',
