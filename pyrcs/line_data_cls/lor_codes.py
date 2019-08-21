dc54a29f4f58536cffedf4bc278bdf00f761dd9b
mikeqfu <qian.fu@outlook.com>
2019-08-21 22:09:11 +0100

    Update lor_codes.py

--- pyrcs/line_data_cls/lor_codes.py
+++ pyrcs/line_data_cls/lor_codes.py
@@ -29,7 +29,7 @@ class LOR:
     def __init__(self, data_dir=None):
         self.HomeURL = 'http://www.railwaycodes.org.uk'
         self.Name = 'Line of Route (LOR/PRIDE) codes'
-        self.URL = 'http://www.railwaycodes.org.uk/pride/pride0.shtm'
+        self.URL = self.HomeURL + '/pride/pride0.shtm'
         self.Catalogue = get_cls_catalogue(self.URL)
         self.Date = get_last_updated_date(self.URL, parsed=True, date_type=False)
         self.DataDir = regulate_input_data_dir(data_dir) if data_dir else cd_dat("Line data", "Line of route codes")
