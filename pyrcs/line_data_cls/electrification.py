fe56b6d723d025ccf364a0aeb95a0d68362865b5
mikeqfu <qian.fu@outlook.com>
2019-08-21 22:07:48 +0100

    Update electrification.py

--- pyrcs/line_data_cls/electrification.py
+++ pyrcs/line_data_cls/electrification.py
@@ -17,11 +17,11 @@ from pyrcs.utils import cd_dat, get_cls_catalogue, get_last_updated_date
 class Electrification:
     def __init__(self, data_dir=None):
         self.HomeURL = 'http://www.railwaycodes.org.uk'
-        self.Name = 'Electrification masts and related features'
-        self.URL = 'http://www.railwaycodes.org.uk/electrification/mast_prefix0.shtm'
+        self.Name = 'Electrification'
+        self.URL = self.HomeURL + '/electrification/mast_prefix0.shtm'
         self.Catalogue = get_cls_catalogue(self.URL)
         self.Date = get_last_updated_date(self.URL, parsed=True, date_type=False)
-        self.DataDir = regulate_input_data_dir(data_dir) if data_dir else cd_dat("Line data", "Electrification")
+        self.DataDir = regulate_input_data_dir(data_dir) if data_dir else cd_dat("Line data", self.Name)
 
     # Change directory to "dat\\Line data\\Electrification" and sub-directories
     def cd_elec(self, *sub_dir):
@@ -36,3 +36,5 @@ class Electrification:
         for x in sub_dir:
             path = os.path.join(path, x)
         return path
+
+# To be updated.
