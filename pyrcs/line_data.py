25bbc2aaeb3d4a2e8a993d606cde976fbba419cc
mikeqfu <qian.fu@outlook.com>
2019-08-28 22:31:19 +0100

    Modified line_data.py

--- pyrcs/line_data.py
+++ pyrcs/line_data.py
@@ -1,8 +1,8 @@
 """ Line data """
 
-from pyrcs.line_data_cls import crs_nlc_tiploc_stanox, elrs_mileages, line_names, lor_codes
-from pyrcs.line_data_cls import electrification, track_diagrams
-from pyrcs.utils import get_cls_catalogue, get_last_updated_date
+from pyrcs.line_data_cls import crs_nlc_tiploc_stanox, electrification, elrs_mileages, lor_codes
+from pyrcs.line_data_cls import line_names, track_diagrams
+from pyrcs.utils import get_cls_menu
 
 
 class LineData:
@@ -10,13 +10,12 @@ class LineData:
         # Basic info
         self.HomeURL = 'http://www.railwaycodes.org.uk'
         self.Name = 'Line data'
-        self.URL = 'http://www.railwaycodes.org.uk/linedatamenu.shtm'
-        self.Catalogue = get_cls_catalogue(self.URL, navigation_bar_exists=False)
-        self.Date = get_last_updated_date(self.URL, parsed=True, date_type=False)
+        self.URL = self.HomeURL + '/linedatamenu.shtm'
+        self.Catalogue = get_cls_menu(self.URL)
         # Classes
         self.ELRMileages = elrs_mileages.ELRMileages()
+        self.Electrification = electrification.Electrification()
         self.LocationIdentifiers = crs_nlc_tiploc_stanox.LocationIdentifiers()
         self.LOR = lor_codes.LOR()
         self.LineNames = line_names.LineNames()
-        self.Electrification = electrification.Electrification()
         self.TrackDiagrams = track_diagrams.TrackDiagrams()
