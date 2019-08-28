1c4d4648e2bc65f65ecb8abfe6404b85a0ac1fc0
mikeqfu <qian.fu@outlook.com>
2019-08-28 22:31:28 +0100

    Modified other_assets.py

--- pyrcs/other_assets.py
+++ pyrcs/other_assets.py
@@ -1,26 +1,20 @@
-#
-import os
-import urllib.parse
+""" Other assets """
 
-import bs4
-import requests
-
-from pyrcs.other_assets_cls import depots, signal_boxes, stations, tunnels, viaducts
+from pyrcs.other_assets_cls import depots, features, signal_boxes, stations, tunnels, viaducts
+from pyrcs.utils import get_cls_menu
 
 
 class OtherAssets:
     def __init__(self):
+        # Basic info
+        self.HomeURL = 'http://www.railwaycodes.org.uk'
         self.Name = 'Other assets'
-        self.URL = 'http://www.railwaycodes.org.uk/otherassetsmenu.shtm'
-        self.HomeURL = os.path.split(self.URL)[0]
-
-        source = requests.get(self.URL)
-        soup = bs4.BeautifulSoup(source.text, 'lxml').find_all('a', text=True)[-6:]
-        contents = [{x.text: urllib.parse.urljoin(self.HomeURL, x['href'])} for x in soup]
-        self.contents = dict(e for d in contents for e in d.items())
-
+        self.URL = self.HomeURL + '/otherassetsmenu.shtm'
+        self.Catalogue = get_cls_menu(self.URL)
+        # Classes
         self.SignalBoxes = signal_boxes.SignalBoxes()
         self.Tunnels = tunnels.Tunnels()
         self.Viaducts = viaducts.Viaducts()
         self.Stations = stations.Stations()
         self.Depots = depots.Depots()
+        self.Features = features.Features()
