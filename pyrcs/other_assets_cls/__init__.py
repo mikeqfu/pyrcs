4b74f1657a461f2a4b56829c82f9d6850f7c771c
mikeqfu <qian.fu@outlook.com>
2020-07-06 13:23:59 +0100

    modified other_assets_cls/__init__.py

--- pyrcs/other_assets_cls/__init__.py
+++ pyrcs/other_assets_cls/__init__.py
@@ -1,1 +1,9 @@
+from .depots import Depots
+from .features import Features
+from .signal_boxes import SignalBoxes
+from .stations import Stations
+from .tunnels import Tunnels
+from .viaducts import Viaducts
+
+
 __all__ = ['depots', 'features', 'signal_boxes', 'stations', 'tunnels', 'viaducts']
