81a11c6d647a999029721303dc7d2066d8bdd303
mikeqfu <qian.fu@outlook.com>
2020-07-11 14:01:11 +0100

    modified viaducts.py and docstrings

--- pyrcs/other_assets_cls/viaducts.py
+++ pyrcs/other_assets_cls/viaducts.py
@@ -1,116 +1,180 @@
-"""
+""" A class for collecting codes of railway viaducts.
 
-Data source: http://www.railwaycodes.org.uk
-
-Railway viaducts (http://www.railwaycodes.org.uk/tunnels/tunnels0.shtm)
-
+Data source: http://www.railwaycodes.org.uk/tunnels/tunnels0.shtm
 """
 
 import copy
+import itertools
 import os
 import re
 
-import bs4
-import fuzzywuzzy.process
 import pandas as pd
-import requests
 from pyhelpers.dir import regulate_input_data_dir
-from pyhelpers.store import load_pickle
+from pyhelpers.store import load_pickle, save_pickle
+from pyhelpers.text import find_similar_str
 
-from pyrcs.utils import cd_dat, get_catalogue, get_last_updated_date
-from pyrcs.utils import save_pickle
+from pyrcs.utils import cd_dat, get_catalogue, get_last_updated_date, homepage_url
 
 
 class Viaducts:
+    """
+    A class for collecting `railway viaducts`_.
+
+    .. _`railway viaducts`: http://www.railwaycodes.org.uk/viaducts/viaducts0.shtm
+
+    :param data_dir: name of data directory, defaults to ``None``
+    :type data_dir: str, None
+
+    **Example**::
+
+        from pyrcs.other_assets import Viaducts
+
+        viaducts = Viaducts()
+
+        print(viaducts.Name)
+        # Railway viaducts
+
+        print(viaducts.SourceURL)
+        # http://www.railwaycodes.org.uk/viaducts/viaducts0.shtm
+    """
+
     def __init__(self, data_dir=None):
-        self.HomeURL = 'http://www.railwaycodes.org.uk'
-        self.Name = 'Viaducts'
-        self.URL = self.HomeURL + '/viaducts/viaducts0.shtm'
-        self.Catalogue = get_catalogue(self.URL)
-        self.Date = get_last_updated_date(self.URL, parsed=True, date_type=False)
-        self.DataDir = regulate_input_data_dir(data_dir) if data_dir else cd_dat("other-assets", "viaducts")
+        """
+        Constructor method.
+        """
+        self.Name = 'Railway viaducts'
+        self.Key = 'Viaducts'
+        self.LUDKey = 'Last updated date'
+        self.HomeURL = homepage_url()
+        self.SourceURL = self.HomeURL + '/viaducts/viaducts0.shtm'
+        self.Catalogue = get_catalogue(self.SourceURL, confirmation_required=False)
+        self.P1Key, self.P2Key, self.P3Key, self.P4Key, self.P5Key, self.P6Key = list(self.Catalogue.keys())[1:]
+        self.Date = get_last_updated_date(self.SourceURL, parsed=True, as_date_type=False)
+        self.DataDir = regulate_input_data_dir(data_dir) if data_dir else cd_dat("other-assets", self.Key.lower())
         self.CurrentDataDir = copy.copy(self.DataDir)
 
-    # Change directory to "dat\\other-assets\\viaducts\\" and sub-directories
-    def cd_viaducts(self, *sub_dir):
+    def cdd_viaducts(self, *sub_dir):
+        """
+        Change directory to "dat\\other-assets\\viaducts\\" and sub-directories (and/or a file)
+
+        :param sub_dir: sub-directory or sub-directories (and/or a file)
+        :type sub_dir: str
+        :return: path to the backup data directory for ``Viaducts``
+        :rtype: str
+
+        :meta private:
+        """
+
         path = self.DataDir
         for x in sub_dir:
             path = os.path.join(path, x)
         return path
 
-    # Change directory to "dat\\other-assets\\viaducts\\dat" and sub-directories
-    def cdd_viaducts(self, *sub_dir):
-        path = self.cd_viaducts("dat")
-        for x in sub_dir:
-            path = os.path.join(path, x)
-        return path
-
-    # Get the titles for each page
-    def get_page_titles(self):
-        intro_page = requests.get(self.URL)
-        pages = [x.text for x in bs4.BeautifulSoup(intro_page.text, 'lxml').find_all('a', text=re.compile('^Page.*'))]
-        pages = pages[0:int(len(pages) / 2)]
-        return pages
-
-    # Collect viaducts data for a given page number
-    def collect_railway_viaducts(self, page_no, update=False, verbose=False):
+    def collect_railway_viaducts_by_page(self, page_no, update=False, verbose=False):
         """
-        :param page_no: [int] page number; valid values include 1, 2, 3, 4, 5, and 6
-        :param update: [bool]
-        :param verbose: [bool]
-        :return: [dict]
+        Collect data of railway viaducts for a given page number from source web page.
+
+        :param page_no: page number; valid values include 1, 2, 3, 4, 5, and 6
+        :type page_no: int, str
+        :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+        :type update: bool
+        :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
+        :type verbose: bool, int
+        :return: railway viaducts data of the given ``page_no`` and date of when the data was last updated
+        :rtype: dict
+
+        **Example**::
+
+            from pyrcs.other_assets import Viaducts
+
+            viaducts = Viaducts()
+
+            update = True
+            verbose = True
+
+            page_no = 1
+            railway_viaducts_1 = viaducts.collect_railway_viaducts_by_page(page_no, update, verbose)
+            print(railway_viaducts_1)
+            # {'Page 1 (A-C)': <codes>,
+            #  'Last updated date': <date>}
         """
+
         assert page_no in range(1, 7), "Valid \"page_no\" must be one of 1, 2, 3, 4, 5, and 6."
-        page_headers = self.get_page_titles()
-        filename = fuzzywuzzy.process.extractOne(str(page_no), page_headers)[0]
-        pickle_filename = re.sub(r"[()]", "", re.sub(r"[ -]", "-", filename)).lower() + ".pickle"
 
-        path_to_pickle = self.cd_viaducts(pickle_filename)
+        page_name = find_similar_str(str(page_no), list(self.Catalogue.keys()))
+
+        pickle_filename = re.sub(r"[()]", "", re.sub(r"[ -]", "-", page_name)).lower() + ".pickle"
+        path_to_pickle = self.cdd_viaducts(pickle_filename)
 
         if os.path.isfile(path_to_pickle) and not update:
-            viaducts_codes = load_pickle(path_to_pickle)
+            page_railway_viaducts = load_pickle(path_to_pickle, verbose=verbose)
 
         else:
-            url = self.URL.replace('viaducts0', 'viaducts{}'.format(page_no))
+            url = self.Catalogue[page_name]
 
             try:
                 last_updated_date = get_last_updated_date(url)
             except Exception as e:
-                print("Failed to find the last updated date for viaducts codes on \"{}\". {}".format(filename, e))
-                last_updated_date = ''
+                print("Failed to find the last updated date for viaducts data of {}. {}".format(page_name, e))
+                last_updated_date = None
 
             try:
                 header, viaducts_table = pd.read_html(url, na_values=[''], keep_default_na=False)
                 viaducts_table.columns = header.columns.to_list()
                 viaducts_table.fillna('', inplace=True)
             except Exception as e:
-                print("Failed to collect viaducts data for Page \"{}\". {}".format(page_no, e))
-                viaducts_table = pd.DataFrame()
+                print("Failed to collect viaducts data of {}. {}".format(page_name, e))
+                viaducts_table = None
 
-            viaducts_codes = {re.search(r'(?<=\()\w.*(?=\))', filename).group(0).replace('-', '_'): viaducts_table}
-            viaducts_codes.update({'Last_updated_date': last_updated_date})
+            page_railway_viaducts = {page_name: viaducts_table, self.LUDKey: last_updated_date}
 
-            save_pickle(viaducts_codes, path_to_pickle, verbose)
+            save_pickle(page_railway_viaducts, path_to_pickle, verbose=verbose)
 
-        return viaducts_codes
+        return page_railway_viaducts
 
-    # Fetch all of the collected viaducts data
     def fetch_railway_viaducts(self, update=False, pickle_it=False, data_dir=None, verbose=False):
+        """
+        Fetch data of railway viaducts from local backup.
 
-        data_sets = [self.collect_railway_viaducts(page_no, update, verbose=False if data_dir or not verbose else True)
-                     for page_no in range(1, 7)]
+        :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+        :type update: bool
+        :param pickle_it: whether to replace the current package data with newly collected data, defaults to ``False``
+        :type pickle_it: bool
+        :param data_dir: name of package data folder, defaults to ``None``
+        :type data_dir: str, None
+        :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
+        :type verbose: bool, int
+        :return railway viaducts data and date of when the data was last updated
+        :rtype: dict
 
-        viaducts_codes_tables = [dat[k] for dat in data_sets for k, v in dat.items() if k != 'Last_updated_date']
-        viaducts_codes = pd.concat(viaducts_codes_tables, axis=0, ignore_index=True, sort=False)
+        **Example**::
 
-        last_updated_dates = [dat[k] for dat in data_sets for k, v in dat.items() if k == 'Last_updated_date']
-        latest_update_date = max(last_updated_dates)
+            from pyrcs.other_assets import Viaducts
 
-        viaducts_codes = {'Viaducts': viaducts_codes, 'Latest_update_date': latest_update_date}
+            viaducts = Viaducts()
+
+            update = False
+            pickle_it = False
+            data_dir = None
+            verbose = False
+
+            railway_viaducts = viaducts.fetch_railway_viaducts(update, pickle_it, data_dir, verbose)
+
+            print(railway_tunnel_lengths)
+            # {'Viaducts': <codes>,
+            #  'Latest update date': <date>}
+        """
+
+        verbose_ = False if data_dir or not verbose else True
+        codes = [self.collect_railway_viaducts_by_page(page_no, update, verbose=verbose_) for page_no in range(1, 7)]
+
+        railways_viaducts_data = {
+            self.Key: {next(iter(x)): next(iter(x.values())) for x in codes},
+            self.LUDKey: max(next(itertools.islice(iter(x.values()), 1, 2)) for x in codes)}
 
         if pickle_it and data_dir:
             self.CurrentDataDir = regulate_input_data_dir(data_dir)
-            path_to_pickle = os.path.join(self.CurrentDataDir, "railway-viaducts.pickle")
-            save_pickle(viaducts_codes, path_to_pickle, verbose=True)
+            path_to_pickle = os.path.join(self.CurrentDataDir, self.Key.lower().replace(" ", "-") + ".pickle")
+            save_pickle(railways_viaducts_data, path_to_pickle, verbose=True)
 
-        return viaducts_codes
+        return railways_viaducts_data
