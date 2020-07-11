cf958baaeaa48905f510b0c611a7e8c9628d2c26
mikeqfu <qian.fu@outlook.com>
2020-07-11 15:23:14 +0100

    modified __init__.py

--- pyrcs/__init__.py
+++ pyrcs/__init__.py
@@ -1,8 +1,8 @@
-from pyrcs.line_data_cls import *
-from pyrcs.other_assets_cls import *
+from pyrcs.line_data import *
+from pyrcs.other_assets import *
 
 __package_name__ = 'pyrcs'
 __version__ = '0.2.7'
 __author__ = 'Qian Fu'
 __email__ = 'qian.fu@outlook.com'
-__description__ = "A web-scraping tool for collecting acronyms and codes being used in Britain's railway."
+__description__ = "A web-scraping tool for collecting codes being used in Britain's railway."
