85fb4915a7d8d84baec3e5cc0e2942b004370b02
mikeqfu <qian.fu@outlook.com>
2020-07-18 15:21:16 +0100

    modified depots.py

--- pyrcs/other_assets/depots.py
+++ pyrcs/other_assets/depots.py
@@ -6,6 +6,7 @@ Data source: http://www.railwaycodes.org.uk/depots/depots0.shtm
 import copy
 import os
 import re
+import urllib.parse
 
 import bs4
 import pandas as pd
@@ -23,6 +24,8 @@ class Depots:
 
     :param data_dir: name of data directory, defaults to ``None``
     :type data_dir: str, None
+    :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+    :type update: bool
 
     **Example**::
 
@@ -37,14 +40,14 @@ class Depots:
         # http://www.railwaycodes.org.uk/depots/depots0.shtm
     """
 
-    def __init__(self, data_dir=None):
+    def __init__(self, data_dir=None, update=False):
         """
         Constructor method.
         """
         self.Name = 'Depot codes'
         self.HomeURL = homepage_url()
-        self.SourceURL = self.HomeURL + '/depots/depots0.shtm'
-        self.Catalogue = get_catalogue(self.SourceURL, confirmation_required=False)
+        self.SourceURL = urllib.parse.urljoin(self.HomeURL, '/depots/depots0.shtm')
+        self.Catalogue = get_catalogue(self.SourceURL, update=update, confirmation_required=False)
         self.Date = get_last_updated_date(self.SourceURL, parsed=True, as_date_type=False)
         self.Key = 'Depots'
         self.LUDKey = 'Last updated date'  # key to last updated date
@@ -80,7 +83,7 @@ class Depots:
         :param confirmation_required: whether to prompt a message for confirmation to proceed, defaults to ``True``
         :type confirmation_required: bool
         :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
-        :type verbose: bool
+        :type verbose: bool, int
         :return: data of two-character TOPS codes and date of when the data was last updated
         :rtype: dict, None
 
@@ -91,10 +94,9 @@ class Depots:
             depots = Depots()
 
             confirmation_required = True
-            verbose = True
 
-            two_char_tops_codes_data = depots.collect_two_char_tops_codes(confirmation_required, verbose)
-            # To collect two character TOPS codes? [No]|Yes:
+            two_char_tops_codes_data = depots.collect_two_char_tops_codes(confirmation_required)
+            # To collect data of two character TOPS codes? [No]|Yes:
             # >? yes
 
             print(two_char_tops_codes_data)
@@ -154,10 +156,8 @@ class Depots:
             update = False
             pickle_it = False
             data_dir = None
-            verbose = True
 
-            two_char_tops_codes_data = depots.fetch_two_char_tops_codes(update, pickle_it, data_dir,
-                                                                        verbose)
+            two_char_tops_codes_data = depots.fetch_two_char_tops_codes(update, pickle_it, data_dir)
 
             print(two_char_tops_codes_data)
             # {'Two character TOPS codes': <codes>,
@@ -167,7 +167,7 @@ class Depots:
         path_to_pickle = self.cdd_depots(self.TCTPickle + ".pickle")
 
         if os.path.isfile(path_to_pickle) and not update:
-            two_char_tops_codes_data = load_pickle(path_to_pickle, verbose=verbose)
+            two_char_tops_codes_data = load_pickle(path_to_pickle)
 
         else:
             two_char_tops_codes_data = self.collect_two_char_tops_codes(
@@ -190,7 +190,7 @@ class Depots:
         :param confirmation_required: whether to prompt a message for confirmation to proceed, defaults to ``True``
         :type confirmation_required: bool
         :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
-        :type verbose: bool
+        :type verbose: bool, int
         :return: data of two-character TOPS codes and date of when the data was last updated
         :rtype: dict, None
 
@@ -201,14 +201,12 @@ class Depots:
             depots = Depots()
 
             confirmation_required = True
-            verbose = True
 
-            four_digit_pre_tops_codes_data = depots.collect_four_digit_pre_tops_codes(
-                confirmation_required, verbose)
-            # To collect four digit pre-TOPS codes? [No]|Yes:
+            four_digit_pre_tops_codes = depots.collect_four_digit_pre_tops_codes(confirmation_required)
+            # To collect data of four digit pre-TOPS codes? [No]|Yes:
             # >? yes
 
-            print(four_digit_pre_tops_codes_data)
+            print(four_digit_pre_tops_codes)
             # {'Four digit pre-TOPS codes': <codes>,
             #  'Last updated date': <date>}
         """
@@ -239,18 +237,18 @@ class Depots:
 
                 last_updated_date = get_last_updated_date(url)
 
-                four_digit_pre_tops_codes_data = {self.FDPTKey: dict(zip(region_names, four_digit_pre_tops_codes_list)),
-                                                  self.LUDKey: last_updated_date}
+                four_digit_pre_tops_codes = {self.FDPTKey: dict(zip(region_names, four_digit_pre_tops_codes_list)),
+                                             self.LUDKey: last_updated_date}
 
                 print("Done. ") if verbose == 2 else ""
 
-                save_pickle(four_digit_pre_tops_codes_data, path_to_pickle, verbose=verbose)
+                save_pickle(four_digit_pre_tops_codes, path_to_pickle, verbose=verbose)
 
             except Exception as e:
                 print("Failed. {}".format(e))
-                four_digit_pre_tops_codes_data = None
+                four_digit_pre_tops_codes = None
 
-            return four_digit_pre_tops_codes_data
+            return four_digit_pre_tops_codes
 
     def fetch_four_digit_pre_tops_codes(self, update=False, pickle_it=False, data_dir=None, verbose=False):
         """
@@ -276,35 +274,33 @@ class Depots:
             update = False
             pickle_it = False
             data_dir = None
-            verbose = True
 
-            four_digit_pre_tops_codes_data = depots.fetch_four_digit_pre_tops_codes(update, pickle_it,
-                                                                                    data_dir, verbose)
+            four_digit_pretops_codes = depots.fetch_four_digit_pre_tops_codes(update, pickle_it, data_dir)
 
-            print(four_digit_pre_tops_codes_data)
-            # {'Four_digit_pre_TOPS_codes': <codes>,
-            #  'Last_updated_date': <date>}
+            print(four_digit_pretops_codes)
+            # {'Four digit pre-TOPS codes': <codes>,
+            #  'Last updated date': <date>}
         """
 
         path_to_pickle = self.cdd_depots(self.FDPTPickle + ".pickle")
 
         if os.path.isfile(path_to_pickle) and not update:
-            four_digit_pre_tops_codes_data = load_pickle(path_to_pickle, verbose=verbose)
+            four_digit_pretops_codes = load_pickle(path_to_pickle)
 
         else:
-            four_digit_pre_tops_codes_data = self.collect_four_digit_pre_tops_codes(
+            four_digit_pretops_codes = self.collect_four_digit_pre_tops_codes(
                 confirmation_required=False, verbose=False if data_dir or not verbose else True)
 
-            if four_digit_pre_tops_codes_data:
+            if four_digit_pretops_codes:
                 if pickle_it and data_dir:
                     self.CurrentDataDir = regulate_input_data_dir(data_dir)
                     path_to_pickle = os.path.join(self.CurrentDataDir, os.path.basename(path_to_pickle))
-                    save_pickle(four_digit_pre_tops_codes_data, path_to_pickle, verbose=verbose)
+                    save_pickle(four_digit_pretops_codes, path_to_pickle, verbose=verbose)
 
             else:
                 print("No data of {} has been collected.".format(self.FDPTKey[:1].lower() + self.FDPTKey[1:]))
 
-        return four_digit_pre_tops_codes_data
+        return four_digit_pretops_codes
 
     def collect_1950_system_codes(self, confirmation_required=True, verbose=False):
         """
@@ -313,7 +309,7 @@ class Depots:
         :param confirmation_required: whether to prompt a message for confirmation to proceed, defaults to ``True``
         :type confirmation_required: bool
         :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
-        :type verbose: bool
+        :type verbose: bool, int
         :return: data of 1950 system (pre-TOPS) codes and date of when the data was last updated
         :rtype: dict, None
 
@@ -324,10 +320,9 @@ class Depots:
             depots = Depots()
 
             confirmation_required = True
-            verbose = True
 
-            system_1950_codes_data = depots.collect_1950_system_codes(confirmation_required, verbose)
-            # To collect 1950 system (pre-TOPS) codes? [No]|Yes:
+            system_1950_codes_data = depots.collect_1950_system_codes(confirmation_required)
+            # To collect data of 1950 system (pre-TOPS) codes? [No]|Yes:
             # >? yes
 
             print(system_1950_codes_data)
@@ -385,9 +380,8 @@ class Depots:
             update = False
             pickle_it = False
             data_dir = None
-            verbose = True
 
-            system_1950_codes_data = depots.fetch_1950_system_codes(update, pickle_it, data_dir, verbose)
+            system_1950_codes_data = depots.fetch_1950_system_codes(update, pickle_it, data_dir)
 
             print(system_1950_codes_data)
             # {'1950 system (pre-TOPS) codes': <codes>,
@@ -397,7 +391,7 @@ class Depots:
         path_to_pickle = self.cdd_depots(self.S1950Pickle + ".pickle")
 
         if os.path.isfile(path_to_pickle) and not update:
-            system_1950_codes_data = load_pickle(path_to_pickle, verbose=verbose)
+            system_1950_codes_data = load_pickle(path_to_pickle)
 
         else:
             system_1950_codes_data = self.collect_1950_system_codes(
@@ -421,7 +415,7 @@ class Depots:
         :param confirmation_required: whether to prompt a message for confirmation to proceed, defaults to ``True``
         :type confirmation_required: bool
         :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
-        :type verbose: bool
+        :type verbose: bool, int
         :return: data of GWR depot codes and date of when the data was last updated
         :rtype: dict, None
 
@@ -432,10 +426,9 @@ class Depots:
             depots = Depots()
 
             confirmation_required = True
-            verbose = True
 
-            gwr_codes_data = depots.collect_gwr_codes(confirmation_required, verbose)
-            # To collect Great Western Railway depot codes? [No]|Yes:
+            gwr_codes_data = depots.collect_gwr_codes(confirmation_required)
+            # To collect data of GWR codes? [No]|Yes:
             # >? yes
 
             print(gwr_codes_data)
@@ -506,9 +499,8 @@ class Depots:
             update = False
             pickle_it = False
             data_dir = None
-            verbose = True
 
-            gwr_codes_data = depots.fetch_gwr_codes(update, pickle_it, data_dir, verbose)
+            gwr_codes_data = depots.fetch_gwr_codes(update, pickle_it, data_dir)
 
             print(gwr_codes_data)
             # {'GWR codes': <codes>,
@@ -518,7 +510,7 @@ class Depots:
         path_to_pickle = self.cdd_depots(self.GWRPickle + ".pickle")
 
         if os.path.isfile(path_to_pickle) and not update:
-            gwr_codes_data = load_pickle(path_to_pickle, verbose=verbose)
+            gwr_codes_data = load_pickle(path_to_pickle)
 
         else:
             gwr_codes_data = self.collect_gwr_codes(
@@ -559,9 +551,8 @@ class Depots:
             update = False
             pickle_it = False
             data_dir = None
-            verbose = True
 
-            depot_codes = depots.fetch_depot_codes(update, pickle_it, data_dir, verbose)
+            depot_codes = depots.fetch_depot_codes(update, pickle_it, data_dir)
 
             print(depot_codes)
             # {'Depots': <codes>,
