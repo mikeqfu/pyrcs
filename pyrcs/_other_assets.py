1ea268e38800d80a30f356a1b53e44aaa3f651ad
mikeqfu <qian.fu@outlook.com>
2020-07-18 15:29:37 +0100

    modified _other_assets.py

--- pyrcs/_other_assets.py
+++ pyrcs/_other_assets.py
@@ -2,22 +2,43 @@
 
 import urllib.parse
 
-from pyrcs.other_assets import *
-from utils import get_category_menu, homepage_url
+from .other_assets import *
+from .utils import get_category_menu, homepage_url
 
 
 class OtherAssets:
-    def __init__(self):
+    """
+
+    :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+    :type update: bool
+
+    **Example**::
+
+        from pyrcs import OtherAssets
+
+        oa = OtherAssets()
+
+        # To get railway station data
+        stations = oa.Stations
+
+        # data of railway stations whose names start with 'A'
+        railway_station_data_a = stations.collect_railway_station_data_by_initial('A')
+    """
+
+    def __init__(self, update=False):
+        """
+        Constructor method.
+        """
         # Basic info
         self.Name = 'Other assets'
         self.HomeURL = homepage_url()
         self.SourceURL = urllib.parse.urljoin(
             self.HomeURL, '{}menu.shtm'.format(self.Name.lower().replace(' ', '')))
-        self.Catalogue = get_category_menu(self.SourceURL)
+        self.Catalogue = get_category_menu(self.SourceURL, update=update, confirmation_required=False)
         # Classes
-        self.SignalBoxes = signal_boxes.SignalBoxes()
-        self.Tunnels = tunnels.Tunnels()
-        self.Viaducts = viaducts.Viaducts()
-        self.Stations = stations.Stations()
-        self.Depots = depots.Depots()
-        self.Features = features.Features()
+        self.SignalBoxes = signal_boxes.SignalBoxes(update=update)
+        self.Tunnels = tunnels.Tunnels(update=update)
+        self.Viaducts = viaducts.Viaducts(update=update)
+        self.Stations = stations.Stations(update=update)
+        self.Depots = depots.Depots(update=update)
+        self.Features = features.Features(update=update)
