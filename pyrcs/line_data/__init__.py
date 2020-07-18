86d0f31d4396519165e7b82c2191b19672d6b480
mikeqfu <qian.fu@outlook.com>
2020-07-18 15:19:12 +0100

    modified line_data/__init__.py

--- pyrcs/line_data/__init__.py
+++ pyrcs/line_data/__init__.py
@@ -1,9 +1,9 @@
-from pyrcs.line_data.crs_nlc_tiploc_stanox import LocationIdentifiers
-from pyrcs.line_data.electrification import Electrification
-from pyrcs.line_data.elrs_mileages import ELRMileages
-from pyrcs.line_data.line_names import LineNames
-from pyrcs.line_data.lor_codes import LOR
-from pyrcs.line_data.track_diagrams import TrackDiagrams
+from .crs_nlc_tiploc_stanox import LocationIdentifiers
+from .electrification import Electrification
+from .elrs_mileages import ELRMileages
+from .line_names import LineNames
+from .lor_codes import LOR
+from .track_diagrams import TrackDiagrams
 
 
 __all__ = ['crs_nlc_tiploc_stanox', 'electrification', 'elrs_mileages', 'line_names', 'lor_codes', 'track_diagrams']
