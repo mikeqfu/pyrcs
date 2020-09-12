c0e43f98b272a01084838c7f6e9de2d20db77171
mikeqfu <qian.fu@outlook.com>
2020-09-12 23:10:32 +0100

    modified cdd_depots() of depots.py

--- pyrcs/other_assets/depots.py
+++ pyrcs/other_assets/depots.py
@@ -11,7 +11,7 @@ import urllib.parse
 import bs4
 import pandas as pd
 import requests
-from pyhelpers.dir import validate_input_data_dir
+from pyhelpers.dir import cd, validate_input_data_dir
 from pyhelpers.ops import confirmed, fake_requests_headers
 from pyhelpers.store import load_pickle, save_pickle
 
@@ -59,21 +59,21 @@ class Depots:
         self.S1950Pickle = re.sub(r' \(|\) | ', '-', self.S1950Key).lower()
         self.GWRPickle = self.GWRKey.replace(" ", "-").lower()
 
-    def cdd_depots(self, *sub_dir):
+    def cdd_depots(self, *sub_dir, **kwargs):
         """
         Change directory to "dat\\other-assets\\depots\\" and sub-directories (and/or a file)
 
         :param sub_dir: sub-directory or sub-directories (and/or a file)
         :type sub_dir: str
+        :param kwargs: optional parameters of `os.makedirs <https://docs.python.org/3/library/os.html#os.makedirs>`_,
+            e.g. ``mode=0o777``
         :return: path to the backup data directory for ``Depots``
         :rtype: str
 
-        :meta private:
         """
 
-        path = self.DataDir
-        for x in sub_dir:
-            path = os.path.join(path, x)
+        path = cd(self.DataDir, *sub_dir, mkdir=True, **kwargs)
+
         return path
 
     def collect_two_char_tops_codes(self, confirmation_required=True, verbose=False):
