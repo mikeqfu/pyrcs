c044f20b40b23633d6d3d1ad3562e599c3d7a713
mikeqfu <qian.fu@outlook.com>
2020-09-12 22:53:07 +0100

    modified cdd_ln() of line_names.py

--- pyrcs/line_data/line_names.py
+++ pyrcs/line_data/line_names.py
@@ -10,7 +10,7 @@ import urllib.parse
 
 import pandas as pd
 import requests
-from pyhelpers.dir import validate_input_data_dir
+from pyhelpers.dir import cd, validate_input_data_dir
 from pyhelpers.ops import confirmed, fake_requests_headers
 from pyhelpers.store import load_pickle, save_pickle
 
@@ -56,21 +56,21 @@ class LineNames:
             self.DataDir = cd_dat("line-data", self.Key.lower().replace(" ", "-"))
         self.CurrentDataDir = copy.copy(self.DataDir)
 
-    def cdd_ln(self, *sub_dir):
+    def cdd_ln(self, *sub_dir, **kwargs):
         """
         Change directory to "dat\\line-data\\line-names" and sub-directories (and/or a file)
 
         :param sub_dir: sub-directory or sub-directories (and/or a file)
         :type sub_dir: str
+        :param kwargs: optional parameters of `os.makedirs`_, e.g. ``mode=0o777``
         :return: path to the backup data directory for ``LineNames``
         :rtype: str
 
         :meta private:
         """
 
-        path = self.DataDir
-        for x in sub_dir:
-            path = os.path.join(path, x)
+        path = cd(self.DataDir, *sub_dir, mkdir=True, **kwargs)
+
         return path
 
     def collect_line_names(self, confirmation_required=True, verbose=False):
