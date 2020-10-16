e2941b5726408eab69361a7acf8f8bd7ea407732
mikeqfu <qian.fu@outlook.com>
2020-10-16 16:59:41 +0100

    revise docstrings for line_names.py

--- pyrcs/line_data/line_names.py
+++ pyrcs/line_data/line_names.py
@@ -1,6 +1,6 @@
-""" Collecting British railway line names.
-
-Data source: http://www.railwaycodes.org.uk/misc/line_names.shtm
+"""
+Collecting
+`British railway line names <http://www.railwaycodes.org.uk/misc/line_names.shtm>`_.
 """
 
 import copy
@@ -22,21 +22,22 @@ class LineNames:
     A class for collecting British railway line names.
 
     :param data_dir: name of data directory, defaults to ``None``
-    :type data_dir: str, None
-    :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+    :type data_dir: str or None
+    :param update: whether to check on update and proceed to update the package data, 
+        defaults to ``False``
     :type update: bool
 
     **Example**::
 
-        from pyrcs.line_data import LineNames
+        >>> from pyrcs.line_data import LineNames
 
-        ln = LineNames()
+        >>> ln = LineNames()
 
-        print(ln.Name)
-        # Railway line names
+        >>> print(ln.Name)
+        Railway line names
 
-        print(ln.SourceURL)
-        # http://www.railwaycodes.org.uk/misc/line_names.shtm
+        >>> print(ln.SourceURL)
+        http://www.railwaycodes.org.uk/misc/line_names.shtm
     """
 
     def __init__(self, data_dir=None, update=False):
@@ -46,23 +47,31 @@ class LineNames:
         self.Name = 'Railway line names'
         self.HomeURL = homepage_url()
         self.SourceURL = urllib.parse.urljoin(self.HomeURL, '/misc/line_names.shtm')
-        self.Catalogue = get_catalogue(self.SourceURL, update=update, confirmation_required=False)
+
+        self.Catalogue = get_catalogue(
+            self.SourceURL, update=update, confirmation_required=False)
+
         self.Date = get_last_updated_date(self.SourceURL, parsed=True, as_date_type=False)
         self.Key = 'Line names'
         self.LUDKey = 'Last updated date'
+
         if data_dir:
             self.DataDir = validate_input_data_dir(data_dir)
         else:
             self.DataDir = cd_dat("line-data", self.Key.lower().replace(" ", "-"))
+
         self.CurrentDataDir = copy.copy(self.DataDir)
 
     def cdd_ln(self, *sub_dir, **kwargs):
         """
-        Change directory to "dat\\line-data\\line-names" and sub-directories (and/or a file)
+        Change directory to package data directory and sub-directories (and/or a file).
+
+        The directory for this module: ``"\\dat\\line-data\\line-names"``.
 
         :param sub_dir: sub-directory or sub-directories (and/or a file)
         :type sub_dir: str
-        :param kwargs: optional parameters of `os.makedirs <https://docs.python.org/3/library/os.html#os.makedirs>`_,
+        :param kwargs: optional parameters of
+            `os.makedirs <https://docs.python.org/3/library/os.html#os.makedirs>`_,
             e.g. ``mode=0o777``
         :return: path to the backup data directory for ``LineNames``
         :rtype: str
@@ -78,28 +87,28 @@ class LineNames:
         """
         Collect data of railway line names from source web page.
 
-        :param confirmation_required: whether to require users to confirm and proceed, defaults to ``True``
+        :param confirmation_required: whether to require users to confirm and proceed, 
+            defaults to ``True``
         :type confirmation_required: bool
-        :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
+        :param verbose: whether to print relevant information in console 
+            as the function runs, defaults to ``False``
         :type verbose: bool
-        :return: railway line names and routes data and date of when the data was last updated
-        :rtype: dict, None
+        :return: railway line names and routes data and 
+            date of when the data was last updated
+        :rtype: dict or None
 
         **Example**::
 
-            from pyrcs.line_data import LineNames
+            >>> from pyrcs.line_data import LineNames
 
-            ln = LineNames()
+            >>> ln = LineNames()
 
-            confirmation_required = True
+            >>> line_names_dat = ln.collect_line_names(confirmation_required=False)
 
-            line_names_data = ln.collect_line_names(confirmation_required)
-            # To collect British railway line names? [No]|Yes:
-            # >? yes
-
-            print(line_names_data)
-            # {'Line names': <code>,
-            #  'Last updated date': <date>}
+            >>> type(line_names_dat)
+            <class 'dict'>
+            >>> print(list(line_names_dat.keys()))
+            ['Line names', 'Last updated date']
         """
 
         if confirmed("To collect British railway {}?".format(self.Key.lower()),
@@ -111,8 +120,9 @@ class LineNames:
             try:
                 source = requests.get(self.SourceURL, headers=fake_requests_headers())
                 row_lst, header = parse_table(source, parser='lxml')
-                line_names = pd.DataFrame([[r.replace('\xa0', '').strip() for r in row] for row in row_lst],
-                                          columns=header)
+                line_names = pd.DataFrame(
+                    [[r.replace('\xa0', '').strip() for r in row] for row in row_lst],
+                    columns=header)
 
                 # Parse route column
                 def parse_route_column(x):
@@ -134,7 +144,8 @@ class LineNames:
                         route, route_note = x, None
                     return route, route_note
 
-                line_names[['Route', 'Route_note']] = line_names.Route.map(parse_route_column).apply(pd.Series)
+                line_names[['Route', 'Route_note']] = \
+                    line_names.Route.map(parse_route_column).apply(pd.Series)
 
                 last_updated_date = get_last_updated_date(self.SourceURL)
 
@@ -152,36 +163,38 @@ class LineNames:
 
             return line_names_data
 
-    def fetch_line_names(self, update=False, pickle_it=False, data_dir=None, verbose=False):
+    def fetch_line_names(self, update=False, pickle_it=False, data_dir=None,
+                         verbose=False):
         """
         Fetch data of railway line names from local backup.
 
-        :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+        :param update: whether to check on update and proceed to update the package data, 
+            defaults to ``False``
         :type update: bool
-        :param pickle_it: whether to replace the current package data with newly collected data, defaults to ``False``
+        :param pickle_it: whether to replace the current package data 
+            with newly collected data, defaults to ``False``
         :type pickle_it: bool
         :param data_dir: name of package data folder, defaults to ``None``
-        :type data_dir: str, None
-        :param verbose: whether to print relevant information in console as the function runs, defaults to ``False``
+        :type data_dir: str or None
+        :param verbose: whether to print relevant information in console 
+            as the function runs, defaults to ``False``
         :type verbose: bool
-        :return: railway line names and routes data and date of when the data was last updated
+        :return: railway line names and routes data and 
+            date of when the data was last updated
         :rtype: dict
 
         **Example**::
 
-            from pyrcs.line_data import LineNames
+            >>> from pyrcs.line_data import LineNames
 
-            ln = LineNames()
+            >>> ln = LineNames()
 
-            update = False
-            pickle_it = False
-            data_dir = None
+            >>> line_names_dat = ln.fetch_line_names()
 
-            line_names_data = ln.fetch_line_names(update, pickle_it, data_dir)
-
-            print(line_names_data)
-            # {'Line names': <code>,
-            #  'Last updated date': <date>}
+            >>> type(line_names_dat)
+            <class 'dict'>
+            >>> print(list(line_names_dat.keys()))
+            ['Line names', 'Last updated date']
         """
 
         pickle_filename = self.Key.lower().replace(" ", "-") + ".pickle"
@@ -191,14 +204,16 @@ class LineNames:
             line_names_data = load_pickle(path_to_pickle)
 
         else:
-            line_names_data = self.collect_line_names(confirmation_required=False,
-                                                      verbose=False if data_dir or not verbose else True)
+            line_names_data = self.collect_line_names(
+                confirmation_required=False,
+                verbose=False if data_dir or not verbose else True)
             if line_names_data:  # line-names is not None
                 if pickle_it and data_dir:
                     self.CurrentDataDir = validate_input_data_dir(data_dir)
                     path_to_pickle = os.path.join(self.CurrentDataDir, pickle_filename)
                     save_pickle(line_names_data, path_to_pickle, verbose=verbose)
             else:
-                print("No data of the railway {} has been collected.".format(self.Key.lower()))
+                print("No data of the railway {} has been collected.".format(
+                    self.Key.lower()))
 
         return line_names_data
