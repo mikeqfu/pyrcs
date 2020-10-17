7dd79e4bc13c392df0a314da71d86dcb6dd69421
mikeqfu <qian.fu@outlook.com>
2020-10-17 20:49:50 +0100

    revise docstrings for _line_data.py

--- pyrcs/_line_data.py
+++ pyrcs/_line_data.py
@@ -1,4 +1,6 @@
-""" Collecting line data """
+"""
+Collecting `line data <http://www.railwaycodes.org.uk/linedatamenu.shtm>`_.
+"""
 
 import urllib.parse
 
@@ -8,20 +10,51 @@ from .utils import get_category_menu, homepage_url
 
 class LineData:
     """
-    :param update: whether to check on update and proceed to update the package data, defaults to ``False``
+    :param update: whether to check on update and proceed to update the package data,
+        defaults to ``False``
     :type update: bool
 
     **Example**::
 
-        from pyrcs import LineData
+        >>> from pyrcs import LineData
 
-        ld = LineData()
+        >>> ld = LineData()
 
-        # To get location codes
-        lid = ld.LocationIdentifiers
+        >>> # To get location codes
+        >>> location_codes_data = ld.LocationIdentifiers.fetch_location_codes()
 
-        # location codes that start with 'A'
-        location_codes_a = lid.collect_location_codes_by_initial('A')
+        >>> type(location_codes_data)
+        <class 'dict'>
+        >>> print(list(location_codes_data.keys()))
+        ['Location codes', 'Other systems', 'Additional notes', 'Last updated date']
+        >>> location_codes_dat = location_codes_data['Location codes']
+        >>> print(location_codes_dat.head())
+                                       Location CRS  ... STANME_Note STANOX_Note
+        0                                Aachen      ...
+        1                    Abbeyhill Junction      ...
+        2                 Abbeyhill Signal E811      ...
+        3            Abbeyhill Turnback Sidings      ...
+        4  Abbey Level Crossing (Staffordshire)      ...
+
+        [5 rows x 12 columns]
+
+        >>> # To get location codes
+        >>> line_names_data = ld.LineNames.fetch_line_names()
+
+        >>> type(line_names_data)
+        <class 'dict'>
+        >>> print(list(line_names_data.keys()))
+        ['Line names', 'Last updated date']
+        >>> line_names_dat = line_names_data['Line names']
+        >>> print(line_names_dat.head())
+                     Line name  ... Route_note
+        0           Abbey Line  ...       None
+        1        Airedale Line  ...       None
+        2          Argyle Line  ...       None
+        3     Arun Valley Line  ...       None
+        4  Atlantic Coast Line  ...       None
+
+        [5 rows x 3 columns]
     """
 
     def __init__(self, update=False):
@@ -33,11 +66,13 @@ class LineData:
         self.HomeURL = homepage_url()
         self.SourceURL = urllib.parse.urljoin(
             self.HomeURL, '{}menu.shtm'.format(self.Name.lower().replace(' ', '')))
-        self.Catalogue = get_category_menu(self.SourceURL, update=update, confirmation_required=False)
+        self.Catalogue = \
+            get_category_menu(self.SourceURL, update=update, confirmation_required=False)
         # Classes
         self.ELRMileages = elrs_mileages.ELRMileages(update=update)
         self.Electrification = electrification.Electrification(update=update)
-        self.LocationIdentifiers = crs_nlc_tiploc_stanox.LocationIdentifiers(update=update)
+        self.LocationIdentifiers = \
+            crs_nlc_tiploc_stanox.LocationIdentifiers(update=update)
         self.LOR = lor_codes.LOR(update=update)
         self.LineNames = line_names.LineNames(update=update)
         self.TrackDiagrams = track_diagrams.TrackDiagrams(update=update)
