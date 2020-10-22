b1de8e851575dde1d768b8856eb0c5873eae6aba
mikeqfu <qian.fu@outlook.com>
2020-10-22 22:21:18 +0100

    update \tests\test_station.py

--- tests/test_station.py
+++ tests/test_station.py
@@ -1,4 +1,4 @@
-from pyrcs.other_assets.stations import Stations
+from pyrcs.other_assets.station import Stations
 
 if __name__ == '__main__':
 
@@ -23,11 +23,13 @@ if __name__ == '__main__':
 
     print("")
 
-    railway_station_data2 = stations.fetch_railway_station_data(update=True, pickle_it=True, data_dir=test_data_dir)
+    railway_station_data2 = stations.fetch_railway_station_data(
+        update=True, pickle_it=True, data_dir=test_data_dir)
     # No data is available for signal box codes beginning with "X".
     # No data is available for signal box codes beginning with "Z".
 
     railway_station_data2_ = railway_station_data2[stations.StnKey]
 
-    print("\n{} data is consistent:".format(stations.StnKey), railway_station_data1_.equals(railway_station_data2_))
+    print("\n{} data is consistent:".format(stations.StnKey),
+          railway_station_data1_.equals(railway_station_data2_))
     # Railway station data is consistent: True
