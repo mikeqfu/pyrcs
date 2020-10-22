f49d6396408774fe28cd7b0d31f1f63c81970f14
mikeqfu <qian.fu@outlook.com>
2020-10-22 22:19:50 +0100

    update \tests\test_lor_code.py

--- tests/test_lor_code.py
+++ tests/test_lor_code.py
@@ -11,16 +11,19 @@ if __name__ == '__main__':
     elr_lor_converter1 = lor.fetch_elr_lor_converter()
     elr_lor_converter1_ = elr_lor_converter1[lor.ELCKey]
 
-    print(f"\n  {lor.ELCKey}:\n    {type(elr_lor_converter1_)}, {elr_lor_converter1_.shape}")
+    print(f"\n  {lor.ELCKey}:\n    {type(elr_lor_converter1_)}, "
+          f"{elr_lor_converter1_.shape}")
     print(f"\n    {lor.LUDKey}: {elr_lor_converter1[lor.LUDKey]}")
     #   ELR/LOR converter:
     #     <class 'pandas.core.frame.DataFrame'>, (1354, 6)
     #
     #     Last updated date: 2019-11-15
 
-    elr_lor_converter2 = lor.fetch_elr_lor_converter(update=True, pickle_it=True, data_dir=test_data_dir)
+    elr_lor_converter2 = lor.fetch_elr_lor_converter(update=True, pickle_it=True,
+                                                     data_dir=test_data_dir)
     elr_lor_converter2_ = elr_lor_converter2[lor.ELCKey]
-    print("\n    {} data is consistent:".format(lor.ELCKey), elr_lor_converter1_.equals(elr_lor_converter2_))
+    print("\n    {} data is consistent:".format(lor.ELCKey),
+          elr_lor_converter1_.equals(elr_lor_converter2_))
     #     ELR/LOR converter data is consistent: True
 
     lor_codes_data1 = lor.fetch_lor_codes()
@@ -61,7 +64,8 @@ if __name__ == '__main__':
     #       XR:
     #         <class 'dict'>
 
-    lor_codes_data2 = lor.fetch_lor_codes(update=True, pickle_it=True, data_dir=test_data_dir)
+    lor_codes_data2 = lor.fetch_lor_codes(update=True, pickle_it=True,
+                                          data_dir=test_data_dir)
     lor_codes_data2_ = lor_codes_data2[lor.Key]
 
     print("\n    {} data is consistent:".format(lor.Key),
