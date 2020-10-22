7ac3815e30ed099aca20e1e5a6e71521c5675958
mikeqfu <qian.fu@outlook.com>
2020-10-22 22:08:27 +0100

    rename test_depots.py to test_depot.py at \tests

--- tests/test_depot.py
+++ tests/test_depot.py
@@ -1,4 +1,4 @@
-from pyrcs.other_assets.depots import Depots
+from pyrcs.other_assets.depot import Depots
 
 if __name__ == '__main__':
     depots = Depots()
@@ -11,14 +11,16 @@ if __name__ == '__main__':
     two_char_tops_codes_data1 = depots.fetch_two_char_tops_codes()
     two_char_tops_codes_data1_ = two_char_tops_codes_data1[depots.TCTKey]
 
-    print(f"\n  {depots.TCTKey}:\n    {type(two_char_tops_codes_data1_)}, {two_char_tops_codes_data1_.shape}")
+    print(f"\n  {depots.TCTKey}:\n    {type(two_char_tops_codes_data1_)}, "
+          f"{two_char_tops_codes_data1_.shape}")
     print(f"\n    {depots.LUDKey}: {two_char_tops_codes_data1[depots.LUDKey]}")
     #   Two character TOPS codes:
     #     <class 'pandas.core.frame.DataFrame'>, (585, 5)
     #
     #     Last updated date: xxxx-xx-xx
 
-    two_char_tops_codes_data2 = depots.fetch_two_char_tops_codes(update=True, pickle_it=True, data_dir=test_data_dir)
+    two_char_tops_codes_data2 = depots.fetch_two_char_tops_codes(
+        update=True, pickle_it=True, data_dir=test_data_dir)
     two_char_tops_codes_data2_ = two_char_tops_codes_data2[depots.TCTKey]
     print("\n    {} data is consistent:".format(depots.TCTKey),
           two_char_tops_codes_data1_.equals(two_char_tops_codes_data2_))
@@ -50,24 +52,27 @@ if __name__ == '__main__':
     #     Scottish Region:
     #       <class 'pandas.core.frame.DataFrame'>, (154, 2)
 
-    four_digit_pre_tops_codes_data2 = depots.fetch_four_digit_pre_tops_codes(update=True, pickle_it=True,
-                                                                             data_dir=test_data_dir)
+    four_digit_pre_tops_codes_data2 = depots.fetch_four_digit_pre_tops_codes(
+        update=True, pickle_it=True, data_dir=test_data_dir)
     four_digit_pre_tops_codes_data2_ = four_digit_pre_tops_codes_data2[depots.FDPTKey]
     print("\n    {} data is consistent:".format(depots.FDPTKey),
-          all((x == y) for x, y in zip(four_digit_pre_tops_codes_data1_, four_digit_pre_tops_codes_data2_)))
+          all((x == y) for x, y in zip(four_digit_pre_tops_codes_data1_,
+                                       four_digit_pre_tops_codes_data2_)))
     #     Four digit pre-TOPS codes data is consistent: True
 
     system_1950_codes_data1 = depots.fetch_1950_system_codes()
     system_1950_codes_data1_ = system_1950_codes_data1[depots.S1950Key]
 
-    print(f"\n  {depots.S1950Key}:\n    {type(system_1950_codes_data1_)}, {system_1950_codes_data1_.shape}")
+    print(f"\n  {depots.S1950Key}:\n    {type(system_1950_codes_data1_)}, "
+          f"{system_1950_codes_data1_.shape}")
     print(f"\n    {depots.LUDKey}: {two_char_tops_codes_data1[depots.LUDKey]}")
     #   1950 system (pre-TOPS) codes:
     #     <class 'pandas.core.frame.DataFrame'>, (625, 3)
     #
     #     Last updated date: 2020-06-23
 
-    system_1950_codes_data2 = depots.fetch_1950_system_codes(update=True, pickle_it=True, data_dir=test_data_dir)
+    system_1950_codes_data2 = depots.fetch_1950_system_codes(
+        update=True, pickle_it=True, data_dir=test_data_dir)
     system_1950_codes_data2_ = system_1950_codes_data2[depots.S1950Key]
     print("\n    {} data is consistent:".format(depots.S1950Key),
           system_1950_codes_data1_.equals(system_1950_codes_data2_))
@@ -87,7 +92,8 @@ if __name__ == '__main__':
     #     Numerical codes:
     #       <class 'pandas.core.frame.DataFrame'>, (145, 2)
 
-    gwr_codes_data2 = depots.fetch_gwr_codes(update=True, pickle_it=True, data_dir=test_data_dir)
+    gwr_codes_data2 = depots.fetch_gwr_codes(
+        update=True, pickle_it=True, data_dir=test_data_dir)
     gwr_codes_data2_ = gwr_codes_data1[depots.GWRKey]
     print("\n    {} data is consistent:".format(depots.GWRKey),
           all((x == y) for x, y in zip(gwr_codes_data1_, gwr_codes_data2_)))
