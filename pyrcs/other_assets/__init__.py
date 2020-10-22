602d94d4d5b61da01ae48ab67479bfd702a8f167
mikeqfu <qian.fu@outlook.com>
2020-10-22 21:49:05 +0100

    update \other_assets\__init__.py

--- pyrcs/other_assets/__init__.py
+++ pyrcs/other_assets/__init__.py
@@ -1,10 +1,16 @@
-from .depots import Depots
-from .features import Features
-from .signal_boxes import SignalBoxes
-from .stations import Stations
-from .tunnels import Tunnels
-from .viaducts import Viaducts
+"""
+A collection of modules for collecting
+`other assets <http://www.railwaycodes.org.uk/otherassetsmenu.shtm>`_. See also
+:py:mod:`pyrcs._other_assets<pyrcs._other_assets>`.
+"""
+
+from .depot import Depots
+from .feature import Features
+from .sig_box import SignalBoxes
+from .station import Stations
+from .tunnel import Tunnels
+from .viaduct import Viaducts
 
 
-__all__ = ['depots', 'features', 'signal_boxes', 'stations', 'tunnels', 'viaducts',
+__all__ = ['depot', 'feature', 'sig_box', 'station', 'tunnel', 'viaduct',
            'Depots', 'Features', 'SignalBoxes', 'Stations', 'Tunnels', 'Viaducts']
