38590083d541c5ac784921f6efc375b0e8399cff
mikeqfu <qian.fu@outlook.com>
2020-10-22 22:20:41 +0100

    update \tests\test_sig_box.py

--- tests/test_sig_box.py
+++ tests/test_sig_box.py
@@ -12,22 +12,25 @@ if __name__ == '__main__':
     signal_box_prefix_codes1 = signal_boxes.fetch_signal_box_prefix_codes()
     signal_box_prefix_codes1_ = signal_box_prefix_codes1[signal_boxes.Key]
 
-    print(f"\n  {signal_boxes.Key}:\n    {type(signal_box_prefix_codes1_)}, {signal_box_prefix_codes1_.shape}")
-    print("\n  {}: {}".format(signal_boxes.LUDKey, signal_box_prefix_codes1[signal_boxes.LUDKey]))
+    print(f"\n  {signal_boxes.Key}:\n    {type(signal_box_prefix_codes1_)}, "
+          f"{signal_box_prefix_codes1_.shape}")
+    print("\n  {}: {}".format(signal_boxes.LUDKey,
+                              signal_box_prefix_codes1[signal_boxes.LUDKey]))
     #   Signal boxes:
     #     <class 'pandas.core.frame.DataFrame'>, (5507, 8)
     #
     #   Last updated date: xxxx-xx-xx
 
-    signal_box_prefix_codes2 = signal_boxes.fetch_signal_box_prefix_codes(update=True, pickle_it=True,
-                                                                          data_dir=test_data_dir)
+    signal_box_prefix_codes2 = signal_boxes.fetch_signal_box_prefix_codes(
+        update=True, pickle_it=True, data_dir=test_data_dir)
     signal_box_prefix_codes2_ = signal_box_prefix_codes2[signal_boxes.Key]
     print("\n  {} data is consistent:".format(signal_boxes.Key),
           signal_box_prefix_codes1_.equals(signal_box_prefix_codes2_))
     #   Signal boxes data is consistent: True
 
     non_national_rail_codes_data1 = signal_boxes.fetch_non_national_rail_codes()
-    non_national_rail_codes_data1_ = non_national_rail_codes_data1[signal_boxes.NonNationalRailKey]
+    non_national_rail_codes_data1_ = \
+        non_national_rail_codes_data1[signal_boxes.NonNationalRailKey]
 
     print("\n\n  {}: ".format(signal_boxes.NonNationalRailKey))
     for k, v in non_national_rail_codes_data1_.items():
@@ -78,12 +81,15 @@ if __name__ == '__main__':
     #     Heritage, minor and miniature railways and other "special" signals:
     #     <class 'pandas.core.frame.DataFrame'>, (128, 4)
 
-    print("\n  {}: {}".format(signal_boxes.LUDKey, non_national_rail_codes_data1[signal_boxes.LUDKey]))
+    print("\n  {}: {}".format(signal_boxes.LUDKey,
+                              non_national_rail_codes_data1[signal_boxes.LUDKey]))
     #  Last updated date: xxxx-xx-xx
 
-    non_national_rail_codes_data2 = signal_boxes.fetch_non_national_rail_codes(update=True, pickle_it=True,
-                                                                               data_dir=test_data_dir)
-    non_national_rail_codes_data2_ = non_national_rail_codes_data2[signal_boxes.NonNationalRailKey]
+    non_national_rail_codes_data2 = signal_boxes.fetch_non_national_rail_codes(
+        update=True, pickle_it=True, data_dir=test_data_dir)
+    non_national_rail_codes_data2_ = \
+        non_national_rail_codes_data2[signal_boxes.NonNationalRailKey]
     print("\n  {} data is consistent:".format(signal_boxes.NonNationalRailKey),
-          all((x == y) for x, y in zip(non_national_rail_codes_data1_, non_national_rail_codes_data2_)))
+          all((x == y) for x, y in
+              zip(non_national_rail_codes_data1_, non_national_rail_codes_data2_)))
     #   Non-National Rail data is consistent: True
