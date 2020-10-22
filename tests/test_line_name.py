0b8b8a592a8d337aff03d5b0bd968d41c04deb00
mikeqfu <qian.fu@outlook.com>
2020-10-22 22:18:45 +0100

    update \tests\test_line_name.py

--- tests/test_line_name.py
+++ tests/test_line_name.py
@@ -11,15 +11,18 @@ if __name__ == '__main__':
     line_names_data1 = line_names.fetch_line_names()
     line_names_data1_ = line_names_data1[line_names.Key]
 
-    print(f"\n  {line_names.Key}:\n    {type(line_names_data1_)}, {line_names_data1_.shape}")
+    print(f"\n  {line_names.Key}:\n    {type(line_names_data1_)}, "
+          f"{line_names_data1_.shape}")
     print(f"\n    {line_names.LUDKey}: {line_names_data1[line_names.LUDKey]}")
     #   Line names:
     #     <class 'pandas.core.frame.DataFrame'>, (125, 3)
     #
     #     Last updated date: 2019-10-16
 
-    line_names_data2 = line_names.fetch_line_names(update=True, pickle_it=True, data_dir=test_data_dir)
+    line_names_data2 = line_names.fetch_line_names(
+        update=True, pickle_it=True, data_dir=test_data_dir)
     line_names_data2_ = line_names_data2[line_names.Key]
 
-    print("\n    {} data is consistent:".format(line_names.Key), line_names_data1_.equals(line_names_data2_))
+    print("\n    {} data is consistent:".format(line_names.Key),
+          line_names_data1_.equals(line_names_data2_))
     #     Line names data is consistent: True
