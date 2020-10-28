9ee6078f7b5674c120c22446639815250fce1887
mikeqfu <qian.fu@outlook.com>
2020-10-28 12:23:01 +0000

    modify \line_data\__init__.py

--- pyrcs/line_data/__init__.py
+++ pyrcs/line_data/__init__.py
@@ -1,7 +1,8 @@
 """
-A collection of modules for collecting
-`line data <http://www.railwaycodes.org.uk/linedatamenu.shtm>`_. See also
-:py:mod:`pyrcs._line_data<pyrcs._line_data>`.
+A collection of modules for collecting `line data`_.
+See also :py:mod:`pyrcs._line_data<pyrcs._line_data>`.
+
+.. _line data: http://www.railwaycodes.org.uk/linedatamenu.shtm
 """
 
 from .elec import Electrification
@@ -11,6 +12,9 @@ from .loc_id import LocationIdentifiers
 from .lor_code import LOR
 from .trk_diagr import TrackDiagrams
 
-__all__ = ['loc_id', 'elec', 'elr_mileage', 'line_name', 'lor_code', 'trk_diagr',
-           'LocationIdentifiers', 'Electrification', 'ELRMileages', 'LineNames',
-           'LOR', 'TrackDiagrams']
+__all__ = ['elec', 'Electrification',
+           'elr_mileage', 'ELRMileages',
+           'line_name', 'LineNames',
+           'loc_id', 'LocationIdentifiers',
+           'lor_code', 'LOR',
+           'trk_diagr', 'TrackDiagrams']
