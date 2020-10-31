d79c615df77e4b29bc0ffc293031477099147dd5
mikeqfu <qian.fu@outlook.com>
2020-10-31 07:47:50 +0000

    update __init__.py

--- pyrcs/__init__.py
+++ pyrcs/__init__.py
@@ -5,7 +5,7 @@ __all__ = ['_line_data', 'LineData', '_other_assets', 'OtherAssets']
 
 __package_name__ = 'pyrcs'
 __package_name_alt__ = 'PyRCS'
-__version__ = '0.2.10'
+__version__ = '0.2.11'
 __author__ = u'Qian Fu'
 __email__ = 'qian.fu@outlook.com'
 __description__ = "A web-scraping tool for collecting railway codes " \
