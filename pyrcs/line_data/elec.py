c3117e2a71ed8eb6ccb8a0e837ec762cf77ce1eb
mikeqfu <qian.fu@outlook.com>
2020-11-10 21:11:32 +0000

    modify elec.py

--- pyrcs/line_data/elec.py
+++ pyrcs/line_data/elec.py
@@ -19,7 +19,7 @@ from pyhelpers.ops import confirmed, fake_requests_headers
 from pyhelpers.store import load_pickle, save_pickle
 
 from pyrcs.utils import cd_dat, get_catalogue, get_last_updated_date, homepage_url, \
-    parse_tr, print_conn_err, is_internet_connected
+    parse_tr, print_conn_err, is_internet_connected, print_connection_error
 
 
 class Electrification:
@@ -52,20 +52,22 @@ class Electrification:
         """
         Constructor method.
         """
+        if not is_internet_connected():
+            print_connection_error(verbose=verbose)
+
         self.Name = 'Electrification masts and related features'  #: Name of data category
+        self.Key = 'Electrification'
 
         self.HomeURL = homepage_url()  #: URL to the homepage
         self.SourceURL = urllib.parse.urljoin(
             self.HomeURL, '/electrification/mast_prefix0.shtm')
 
+        self.LUDKey = 'Last updated date'  #: Key to last updated date
         self.Date = get_last_updated_date(
-            url=self.SourceURL, parsed=True, as_date_type=False, verbose=verbose)
+            url=self.SourceURL, parsed=True, as_date_type=False)
 
         self.Catalogue = get_catalogue(
             page_url=self.SourceURL, update=update, confirmation_required=False)
-
-        self.Key = 'Electrification'
-        self.LUDKey = 'Last updated date'  #: Key to last updated date
 
         if data_dir:
             self.DataDir = validate_input_data_dir(data_dir)
