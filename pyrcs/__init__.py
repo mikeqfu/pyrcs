49fe58a29630aabf15892534ca31a798bd09cb06
mikeqfu <qian.fu@outlook.com>
2020-11-10 22:02:36 +0000

    modify __init__.py

--- pyrcs/__init__.py
+++ pyrcs/__init__.py
@@ -1,12 +1,13 @@
-from ._line_data import *
-from ._other_assets import *
+from .collector import *
 
-__all__ = ['_line_data', 'LineData', '_other_assets', 'OtherAssets']
+__all__ = [
+    'collector', 'LineData', 'OtherAssets',
+    'Electrification', 'ELRMileages', 'LineNames', 'LocationIdentifiers', 'LOR', 'TrackDiagrams',
+    'Depots', 'Features', 'SignalBoxes', 'Stations', 'Tunnels', 'Viaducts']
 
 __package_name__ = 'pyrcs'
 __package_name_alt__ = 'PyRCS'
-__version__ = '0.2.11'
+__version__ = '0.2.12'
 __author__ = u'Qian Fu'
 __email__ = 'qian.fu@outlook.com'
-__description__ = "A web-scraping tool for collecting railway codes " \
-                  "used in different UK rail industry systems."
+__description__ = "A web-scraping tool for collecting railway codes used in different UK rail industry systems."
