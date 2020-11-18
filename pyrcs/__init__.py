397d97bad10a4b21aab8556b38776477d67dbfb9
mikeqfu <qian.fu@outlook.com>
2020-11-18 11:19:51 +0000

    modify __description__ in __init__.py

--- pyrcs/__init__.py
+++ pyrcs/__init__.py
@@ -10,4 +10,4 @@ __package_name_alt__ = 'PyRCS'
 __version__ = '0.2.12'
 __author__ = u'Qian Fu'
 __email__ = 'qian.fu@outlook.com'
-__description__ = "A web-scraping tool for collecting railway codes used in different UK rail industry systems."
+__description__ = "An open-source tool for collecting railway codes used in different UK rail industry systems."
