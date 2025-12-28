85511b744fcec3df0bb6a442aa3626dfa720afc7
Qian Fu <q.fu@bham.ac.uk>
2025-12-28 08:47:08 +0000

    test: Increase coverage for utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,8 @@
 Test the module :py:mod:`pyrcs.utils`.
 """
 
+import unittest.mock
+
 import pandas as pd
 import pytest
 
@@ -15,71 +17,110 @@ def test_home_page_url():
 def test_is_str_float():
     from pyrcs.utils import is_str_float
 
-    assert is_str_float('') is False
-    assert is_str_float('a') is False
+    assert not is_str_float(None)
+    assert not is_str_float('')
+    assert not is_str_float('a')
     assert is_str_float('1')
     assert is_str_float('1.1')
+    assert not is_str_float('nan', finite_only=True)
+    assert is_str_float('inf')
+
+    assert not is_str_float(is_str_float)
 
 
 def test_validate_initial(capfd):
     from pyrcs.utils import validate_initial
 
     assert validate_initial('x') == 'X'
-
     assert validate_initial('x', as_is=True) == 'x'
 
-    with pytest.raises(Exception) as e:
-        validate_initial('xyz')
-        assert str(e.value) == '`x` must be a single letter.'
+    with pytest.raises(TypeError, match="`initial` must be a string"):
+        _ = validate_initial(1)
+
+    with pytest.raises(ValueError, match="it must be a single letter"):
+        _ = validate_initial('xyz')
 
 
-def test_collect_in_fetch_verbose():
-    from pyrcs.utils import collect_in_fetch_verbose
+def test_get_collect_verbosity_for_fetch():
+    from pyrcs.utils import get_collect_verbosity_for_fetch
 
-    assert collect_in_fetch_verbose(data_dir="data", verbose=True) is False
+    assert not get_collect_verbosity_for_fetch(data_dir="data", verbose=True)
+    assert not get_collect_verbosity_for_fetch(data_dir="data", verbose=2)
+
+    assert get_collect_verbosity_for_fetch(data_dir="", verbose=True)
+    assert not get_collect_verbosity_for_fetch(data_dir="", verbose=False)
+    assert get_collect_verbosity_for_fetch(data_dir="", verbose=2) == 2
 
 
-def test_fetch_all_verbose():
-    from pyrcs.utils import fetch_all_verbose
+def test_get_batch_fetch_verbosity():
+    from pyrcs.utils import get_batch_fetch_verbosity
 
-    assert fetch_all_verbose(data_dir="data", verbose=True) is False
+    assert not get_batch_fetch_verbosity(data_dir="data", verbose=True)
+    assert get_batch_fetch_verbosity(data_dir="", verbose=True)
+
+    with unittest.mock.patch(f'pyrcs.utils.is_home_connectable') as mock_connectable:
+        mock_connectable.return_value = False
+        assert not get_batch_fetch_verbosity(data_dir="data", verbose=2)
+        assert not get_batch_fetch_verbosity(data_dir="", verbose=True)
 
 
-def test_confirm_msg():
+def test_format_confirmation_prompt():
     from pyrcs.utils import format_confirmation_prompt
 
     msg = format_confirmation_prompt(data_name="Railway Codes")
     assert msg == 'To collect data of Railway Codes\n?'
 
 
-def test_print_collect_msg(capfd):
-    from pyrcs.utils import print_collect_msg
+@pytest.mark.parametrize('initial', [None, 'abc'])
+@pytest.mark.parametrize('confirmation_required', [True, False])
+def test_print_collection_message(capfd, initial, confirmation_required):
+    from pyrcs.utils import print_collection_message
 
-    print_collect_msg("Railway Codes", verbose=True, confirmation_required=True)
+    data_name = "Railway Codes"
+
+    # print_collection_message(data_name, verbose=True, confirmation_required=confirmation_required)
+    # out, _ = capfd.readouterr()
+    # assert out.startswith('Collecting the data ... ')
+    #
+    # print_collection_message(data_name, verbose=True, confirmation_required=False)
+    # out, _ = capfd.readouterr()
+    # assert out.startswith(f'Collecting the data of {data_name} ... ')
+
+    print_collection_message(
+        data_name=data_name, initial=initial, verbose=True,
+        confirmation_required=confirmation_required)
     out, _ = capfd.readouterr()
-    assert out.startswith('Collecting the data ... ')
+    if initial and not confirmation_required:
+        assert out.startswith(f'Collecting the data of {data_name} ({initial}) ... ')
+    elif confirmation_required:
+        assert out.startswith('Collecting the data ... ')
+    else:
+        assert out.startswith(f'Collecting the data of {data_name} ... ')
 
-    print_collect_msg("Railway Codes", verbose=True, confirmation_required=False)
+
+def test_print_instance_connection_error(capfd):
+    from pyrcs.utils import print_instance_connection_error
+
+    assert print_instance_connection_error(verbose=False) is None
+
+    print_instance_connection_error(verbose=2)
     out, _ = capfd.readouterr()
-    assert out.startswith('Collecting the data of Railway Codes ... ')
+    assert out == 'Failed. The Internet connection is not available.\n'
 
-
-def test_print_inst_conn_err(capfd):
-    from pyrcs.utils import print_inst_conn_err
-
-    print_inst_conn_err(verbose=True)
-    out, _ = capfd.readouterr()
-    assert out == 'The Internet connection is not available.\n'
-
-    print_inst_conn_err(update=True, verbose=True, e='')
+    print_instance_connection_error(update=True, verbose=True)
     out, _ = capfd.readouterr()
     assert out == 'The Internet connection is not available. Failed to update the data.\n'
 
+    with pytest.raises(TypeError, match="exceptions"):
+        print_instance_connection_error(verbose=True, raise_error=True)
+        out, _ = capfd.readouterr()
+        assert out == 'The Internet connection is not available.\n'
 
-def test_print_void_msg(capfd):
-    from pyrcs.utils import print_void_msg
 
-    print_void_msg(data_name="Railway Codes", verbose=True)
+def test_print_void_collection_message(capfd):
+    from pyrcs.utils import print_void_collection_message
+
+    print_void_collection_message(data_name="Railway Codes", verbose=True)
     out, _ = capfd.readouterr()
     assert out == 'No data of "Railway Codes" has been freshly collected.\n'
 
